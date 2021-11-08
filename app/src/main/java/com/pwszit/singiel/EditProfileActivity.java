68c72b27a04f42da61495476ec0434e0d878e020
Mateuszbb01 <mateuszsromek2@gmail.com>
2021-11-08 16:29:01 +0100

    Update edit layout/beckend

--- app/src/main/java/com/pwszit/singiel/EditProfileActivity.java
+++ app/src/main/java/com/pwszit/singiel/EditProfileActivity.java
@@ -2,13 +2,28 @@ package com.pwszit.singiel;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class EditProfileActivity extends AppCompatActivity {
+
+
+    private TextView txtBirthDate, txtName;
+    SharedPreferences preferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_profile);
+
+        txtName = findViewById(R.id.txtName);
+        txtBirthDate = findViewById(R.id.txtBirthDate);
+
+        preferences = getSharedPreferences("SHARED_PREF", MODE_PRIVATE);
+        String name = preferences.getString("NAME", "");
+        String date = preferences.getString("BIRTHDATE", "");
+        txtName.setText(name);
+        txtBirthDate.setText(date);
     }
 }
