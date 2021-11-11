a3972ad14b8493177f057285045c3a2cc0fa5290
wieclawekmaciej <wieclawekmaciej@gmail.com>
2021-11-11 13:57:12 +0100

    changes in db

--- app/src/main/java/com/pwszit/singiel/Constant.java
+++ app/src/main/java/com/pwszit/singiel/Constant.java
@@ -1,7 +1,7 @@
 package com.pwszit.singiel;
 
 public class Constant {
-    public static final String URL = "http://192.168.1.49:8080/";
+    public static final String URL = "http://172.22.80.1:8080/";
     public static final String HOME = URL+"api";
     public static final String LOGIN = HOME+"/auth/login";
     public static final String REGISTER = HOME+"/auth/register";
