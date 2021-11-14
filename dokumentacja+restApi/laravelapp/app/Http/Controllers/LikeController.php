ac0a4b202e9f725a484c659cdfa54765cdea9ddc
Mateuszbb01 <mateuszsromek2@gmail.com>
2021-11-14 18:55:53 +0100

    Finished task S20-13

--- dokumentacja+restApi/laravelapp/app/Http/Controllers/LikeController.php
+++ dokumentacja+restApi/laravelapp/app/Http/Controllers/LikeController.php
@@ -9,36 +9,30 @@ use App\Models\Preferences;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 class LikeController extends Controller
 {
-    
-
-    //! wyszukać wszystkich użytkowników !oprócz siebie-ok i po przeciwnej płci ta ( powiązaniem paired?)
-    // i ich wyświetlić ze zdjeciem i miastem opisem i wiekiem 
-    // za pomocą user_id = Auth::user()->id; wyszukamy wiersze w których jest uzytkownik  
-
-//////////dołożyć wyswietlanie ze jesli juz kogos swipowal nie wyswietli sie 
 
     public function showUser()
     {
         $id = Auth::user()->id;
 
-        $pr = Preferences::find($id);
-        $gender =  $pr->gender;
-        
-        //  $users = Preferences::with('user')
-        //  ->whereNotIn('gender', [$gender])
-        //  ->whereNotIn('user_id', [$id])
-        //  ->get();
+       $pr = Preferences::find($id);
+       $gender =  $pr->gender;
 
-        $users = Preferences::with('user')
+
+        $find_like = Like::where('user_id', $id)->pluck('user_like_id');
+
+        $findd = Preferences::whereNotIn('user_id', $find_like)
         ->whereNotIn('gender', [$gender])
         ->whereNotIn('user_id', [$id])
         ->get();
+
+
         return response()->json([
             'success' => true,
-            'users' => $users
+            'users' => $findd
         ]);
     }
 
@@ -113,5 +107,6 @@ class LikeController extends Controller
         ]);
 
     }
+
 
 }
