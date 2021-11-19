42ea4b957591c1230086dc6ce4111f267d0d5275
bartekt77 <bartektkr7@gmail.com>
2021-11-19 20:10:49 +0100

    Chat-backend-android-v1

---
+++ app/src/main/java/com/pwszit/singiel/URLs.java
@@ -0,0 +1,19 @@
+package com.pwszit.singiel;
+
+
+public class URLs {
+    public static final String ROOT_URL = "http://192.168.0.103/chatdemo/v1/";
+
+
+    public static final String URL_REGISTER = ROOT_URL + "register";
+
+
+    public static final String URL_STORE_TOKEN = ROOT_URL + "storegcmtoken/";
+
+    public static final String URL_STORE_TOKENDR = ROOT_URL + "storelcgcmtoken/";
+    public static final String URL_FETCH_MESSAGES = ROOT_URL + "messages";
+    public static final String URL_FETCH_GETALLSTUDENT= ROOT_URL + "getallstudents";
+    public static final String URL_FETCH_GETALLLECTURER= ROOT_URL + "getalllecturer";
+
+    public static final String URL_SEND_MESSAGE = ROOT_URL + "send";
+}
